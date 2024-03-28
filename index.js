6976edfb1986e8d49a63347c135c5992686a25ce
Fensley <zagalufe@gmail.com>
2024-03-28 11:24:25 -0400

    initial commit

--- index.js
+++ index.js
@@ -9,10 +9,25 @@ form.addEventListener("submit", (event)=>{
    const list = document.createElement("li");
 
     list.textContent =`${item1} -$ ${item2}`;
+       
+    document.getElementById("expense-list").appendChild(list);
 
-    const orderL= document.getElementById("expense-list").appendChild(list);
-
+    list.classList.add("rene");
+  
       // Clear input fields
       document.getElementById("item-name").value = "";
       document.getElementById("item-price").value = "";
-} );
+
+      const ali = document.querySelectorAll(".rene");
+    // console.log(ali)
+    ali.forEach( e=>{
+      // const slice = e.innerHTML.slice(0,1)
+      const total = document.createElement("p") 
+      total.innerText =`total: ${[e.innerText]}`
+
+      const energy = document.getElementById("expense-list-wrapper").appendChild(total);
+
+    } )
+});
+ 
+
